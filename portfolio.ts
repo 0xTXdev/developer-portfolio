6dd1ef01e78c4dbda2ee2c5c9e3f1cc1c43612a9
Haru Tanaka <hanatanya93@gmail.com>
2024-08-28 10:07:35 -0700

    updated

--- portfolio.ts
+++ portfolio.ts
@@ -207,7 +207,7 @@ export const feedbacks: FeedbackType[] = [
       "Haru has a deep understanding of web development technologies and is adept at using them to build scalable, robust, and secure web applications. He has worked on various projects, ranging from small-scale to large-scale, and has shown his ability to work collaboratively with others while maintaining a high level of productivity and quality. Haru is a pleasure to work with, and I have no doubt that he will be an asset to any team or organization. I highly recommend Haru for any Full Stack Django, React.js, or Blockchain development projects, and I wish him all the best in his future endeavors.",
   },
   {
-    name: "Kiyu Okasaki",
+    name: "Giyu Inoue",
     role: "CTO at Rikuten",
     feedback:
       "Very skilled at what he does and has an in depth knowledge of Django and he's always ready to handle any challenges thrown at him. He's been a valuable member of my team and i look forward to working with him in the years to come",
